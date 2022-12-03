eb066cbcda9adab9a42ea5c8e8166d2ce4df9d03
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2022-12-03 14:47:30 +0000

    Update Usage.py
    uncommented

--- Usage.py
+++ Usage.py
@@ -31,17 +31,15 @@ TDDFT.parse_absorption()
 print("Wavelengths:", TDDFT.wavelengths)
 
 
-# =============================================================================
-# print("\n")
-# print("Meisenheimer Complex")
-# Optimization = ORCAParse("Test-cases/MeisenheimerComplex.out")
-# Optimization.parse_dispersion()
-# print("Meisenheimer Complex D4 energy:", Optimization.dispersions[-1])
-# Optimization.parse_free_energy()
-# print("Meisenheimer Complex Gibbs free energy:", Optimization.Gibbs)
-# Optimization.parse_freqs()
-# print("Meisenheimer Complex Frequencies:", Optimization.frequencies)
-# print("Meisenheimer Complex entropies:", Optimization.entropies)
-# print("Meisenheimer Complex enthalpies:", Optimization.enthalpies)
-# =============================================================================
+print("\n")
+print("Meisenheimer Complex")
+Optimization = ORCAParse("Test-cases/MeisenheimerComplex.out")
+Optimization.parse_dispersion()
+print("Meisenheimer Complex D4 energy:", Optimization.dispersions[-1])
+Optimization.parse_free_energy()
+print("Meisenheimer Complex Gibbs free energy:", Optimization.Gibbs)
+Optimization.parse_freqs()
+print("Meisenheimer Complex Frequencies:", Optimization.frequencies)
+print("Meisenheimer Complex entropies:", Optimization.entropies)
+print("Meisenheimer Complex enthalpies:", Optimization.enthalpies)
 
