9db3d8c9c4d17b875c4fa9e6b092fd20e08a6339
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2023-12-03 12:28:40 +0000

    better pypi page

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 exec(open('orca_parser/version.py').read())
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='orca_parser',
     version=__version__,
     description='A module for parse ORCA output files including hessians (.hess) files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/avanteijlingen/ORCA-Parser',
     author='Alexander van Teijlingen',
     author_email='a.vant@linuxmail.org',
